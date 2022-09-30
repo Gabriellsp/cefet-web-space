0e53ef024c9fca718d7f944d886b3745dbdd495e
gabriellsp <gabriel.luis@dtidigital.com.br>
2022-09-30 20:06:34 -0300

    finalizando exercicio3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+let botoesExpadirRetrair = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let botao of botoesExpadirRetrair) {
+    botao.addEventListener('click', function(evento) {
+        let botaoClicado = evento.currentTarget;
+        paragrafo = botaoClicado.parentNode;
+        paragrafo.classList.toggle('expandido');
+        if (botaoClicado.innerHTML == '+') {
+            botaoClicado.innerHTML = '-';
+        } else {
+            botaoClicado.innerHTML = '+';
+        }
+    })
+}
